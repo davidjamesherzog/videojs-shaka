a2ac5865e7e814620825a1d569751f0c535d8bce
Dave Herzog <dherzog@dramafever.com>
2020-03-06 08:01:11 -0500

    Deselect quality menu items when not selected

--- src/quality/quality-menu.js
+++ src/quality/quality-menu.js
@@ -23,14 +23,18 @@ class QualityMenu extends VjsMenu {
         }
       });
     });
+
+    player.on('qualitytrackchange', function() {
+      console.warn('hide menu');
+      me.hide();
+    });
   }
 
   addItem(component) {
     super.addItem(component);
 
-    component.on('click', () => {
+    component.on(['tap', 'click'], () => {
       const children = this.children();
-
       for (let i = 0; i < children.length; i++) {
         const child = children[i];
         if (component !== child) {
