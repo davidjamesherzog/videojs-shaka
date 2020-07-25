4c4b4d577ec00884f4e74115b7fc88cccd04bdb8
Gordon Pendleton <wgordonw1@gmail.com>
2020-07-25 12:06:08 -0400

    added vagrant

---
+++ vagrant/provision/system.sh
@@ -0,0 +1,10 @@
+set -o errexit
+set -o pipefail
+set -o nounset
+shopt -s failglob
+set -o xtrace
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt-get install -y build-essential nodejs
