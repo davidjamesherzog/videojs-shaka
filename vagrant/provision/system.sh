8eb0023fac1284f8ce971a32c21be297167eac45
Gordon Pendleton <wgordonw1@gmail.com>
2020-07-25 12:08:23 -0400

    used node 8 per contribution guide

--- vagrant/provision/system.sh
+++ vagrant/provision/system.sh
@@ -6,5 +6,5 @@ set -o xtrace
 
 export DEBIAN_FRONTEND=noninteractive
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y build-essential nodejs
