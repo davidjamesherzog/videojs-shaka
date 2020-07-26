43e5bcee46c9167a059aec9c43646c56de340567
Gordon Pendleton <wgordonw1@gmail.com>
2020-07-26 15:10:18 -0400

    used tech options for final source selection

--- src/shaka.js
+++ src/shaka.js
@@ -177,6 +177,36 @@ Shaka.supportsTypeNatively = function(type) {
     return result;
 };
 
+/**
+ * Check if the tech can support the given type
+ *
+ * @param {string} type
+ *        The mimetype to check
+ * @return {string} 'probably', 'maybe', or '' (empty string)
+ */
+Shaka.canPlayType = function(type) {
+  const result = Shaka.manifestSourceHandler.canPlayType(type);
+  shaka.log.debug('Shaka.canPlayType | ' + type + ' | ' + result);
+  return result
+};
+
+/**
+ * Check if the tech can support the given source
+ *
+ * @param {Object} source
+ *        The source object
+ * @param {Object} options
+ *        The options passed to the tech
+ * @return {string} 'probably', 'maybe', or '' (empty string)
+ */
+Shaka.canPlaySource = function(source, options) {
+  const localOptions = videojs.mergeOptions(videojs.options, {shaka: options});
+  const result = Shaka.manifestSourceHandler.canHandleSource(source, localOptions);
+  shaka.log.debug('Shaka.canPlaySource | ' + result + ' | ' + JSON.stringify(source, null, 2) + ' | ' + JSON.stringify(options, null, 2) + ' | ' + JSON.stringify(localOptions, null, 2));
+  return result;
+};
+
+
 Shaka.manifestSourceHandler = {};
 
 Shaka.manifestSourceHandler.canHandleSource = function(source, options = {}) {
